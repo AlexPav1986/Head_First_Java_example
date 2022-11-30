3449c5b37bdf87b0aa9eb1c7e8d68f3ba8924c01
Alexandr Pavlov <96660385+AlexPav1986@users.noreply.github.com>
2022-11-30 10:19:01 +0700

    Add files via upload

---
+++ Dog_method/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+    Dog one = new Dog();
+    Dog two = new Dog();
+    Dog three = new Dog();
+
+    one.size = 70;
+    two.size = 45;
+    three.size = 15;
+
+    one.bark(5);
+    two.bark(2);
+    three.bark(1);
+
+    }
+}
